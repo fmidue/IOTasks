fa8b25f10c6fce7aa2039b1d3a5045c66f5b6e96
Oliver Westphal <oliver.westphal@uni-due.de>
2019-07-22 18:00:25 +0200

    hangman on integers is now working

--- src/Test/IOTest/Examples/Hangman.hs
+++ src/Test/IOTest/Examples/Hangman.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 module Test.IOTest.Examples.Hangman where
 
 import Prelude hiding (getLine, putStrLn, print)
@@ -5,38 +7,40 @@ import Prelude hiding (getLine, putStrLn, print)
 import Test.IOTest.IOtt
 import Test.IOTest.Language
 
--- hangmanSpec :: [Int] -> Specification
--- hangmanSpec word =
---   tillE (
---        branch (winCondition <$> getAllI "guessed")
---          nop
---          (writeOutputP [Contains (pure "Correct") "Correct"] <> e)
---     <> writeOutputP [Everything]
---     <> readInputI "guessed" NatTy
---     <> branch ((`elem` word) <$> getCurrentI "guessed")
---          (optional $ writeOutputP [Contains (pure "wrong") "wrong"])
---          (optional $ writeOutputP [Contains (pure "good Guess") "good Guess"])
---     )
---   where
---     winCondition xs = all (`elem` filter (`elem` word) xs) word
---
---
--- -- TODO: improve Specs, so that the comments can be used for real
--- -- mainly multiline "prompts" is what's missing
--- hangmanProg :: TeletypeM m => [Int] -> m ()
--- hangmanProg word = go [] where
---   go guessed
---     | all (`elem` guessed) word = putStrLn "Correct"
---     | otherwise = do
---         putStrLn $ "\n" ++ printWord word guessed ++ "\nwhich number?"
---         x <- read <$> getLine
---         if x `elem` word && x `notElem` guessed
---           then do
---             putStrLn "good Guess!" -- this is optional
---             go (x:guessed)
---           else do
---             putStrLn "wrong!"
---             go guessed
---
--- printWord :: (Eq a, Show a) => [a] -> [a] -> String
--- printWord xs guessed = foldr (\x -> (++) (if x `elem` guessed then show x ++ " " else "_ ")) "" xs
+-- possible elements to build the secret from
+hangmanDomain :: [Int]
+hangmanDomain = [1..100]
+
+hangmanSpec :: [Int] -> Specification
+hangmanSpec word =
+  tillE (
+       branch (winCondition <$> getAll @Int "guessed")
+         nop
+         (writeFixedOutput ["_Correct_"] <> e)
+    <> writeFixedOutput ["_"]
+    <> readInput "guessed" (intValues hangmanDomain)
+    <> branch ((`elem` word) <$> getCurrent @Int "guessed")
+         (optional $ writeFixedOutput ["_wrong_"])
+         (optional $ writeFixedOutput ["_good Guess_"])
+    )
+  where
+    winCondition xs = all (`elem` filter (`elem` word) xs) word
+
+
+hangmanProg :: TeletypeM m => [Int] -> m ()
+hangmanProg word = go [] where
+  go guessed
+    | all (`elem` guessed) word = putStrLn "Correct"
+    | otherwise = do
+        putStrLn $ "\n" ++ printWord word guessed ++ "\nwhich number?"
+        x <- read <$> getLine
+        if x `elem` word && x `notElem` guessed
+          then do
+            putStrLn "good Guess!" -- this is optional
+            go (x:guessed)
+          else do
+            putStrLn "wrong!"
+            go guessed
+
+printWord :: (Eq a, Show a) => [a] -> [a] -> String
+printWord xs guessed = foldr (\x -> (++) (if x `elem` guessed then show x ++ " " else "_ ")) "" xs
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE TypeApplications #-}
 import TestExamples
 
-import Test.Hspec (hspec)
+import Test.IOTest.IOProperty
+import Test.IOTest.IOtt
+import Test.IOTest.Examples.Hangman
+
+import Test.Hspec (hspec, describe)
+import Test.Hspec.QuickCheck (prop)
 
 main :: IO ()
-main = hspec testExamples
-
-    --it "Testing hangman" $
-    --  specProperty (hangmanSpec [2,7,1,4,2,1]) (hangmanProg [2,7,1,4,2,1])
+main = hspec $ do
+  testExamples
+  describe "Testing hangman" $
+    prop "game runs on a random 'words'" $ \xs -> let word = filter (`elem` hangmanDomain) xs in hangmanProg @IOtt word `fulfills` hangmanSpec word
