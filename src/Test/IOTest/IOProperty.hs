298c8f51840295cf300e56a8702508e01f4df207
Oliver Westphal <oliver.westphal@uni-due.de>
2019-07-23 17:34:01 +0200

    better error msgs on test failure

--- src/Test/IOTest/IOProperty.hs
+++ src/Test/IOTest/IOProperty.hs
@@ -11,11 +11,11 @@ import Test.IOTest.Internal.Specification
 import Test.IOTest.Internal.Trace
 import Test.IOTest.Internal.TraceSet
 
+import Control.Arrow ((&&&))
 import Data.Coerce
 
-import Control.Arrow ((&&&))
-import Control.Monad.Trans.Writer.Lazy
 import Test.QuickCheck
+import Text.PrettyPrint
 
 data IOProperty prog spec = Fulfills prog spec | FulfillsNot prog spec
 
@@ -36,12 +36,19 @@ specProperty :: Specification -> IOtt () -> Property
 specProperty spec program =
   let gen = traceGen spec
       prop t = testTrace ((id &&& inputs) t) program
-  in forAll gen prop
+  in forAllShow gen (render . ppNTraceInfo) prop
 
 testTrace :: (NTrace, [String]) -> IOtt () -> Property
 testTrace (tg,ins) p =
   let trace = runProgram ins p
       normalized = normalize trace
-      w = normalized `isCoveredBy` tg
-      (result,msg) = runWriter w
-  in counterexample (msg ++ "\n  program trace: " ++ show normalized) result
+  in case normalized `isCoveredBy` tg of
+      MatchSuccessfull -> property True
+      err -> counterexample (render $
+        hang (text "Actual run:") 4
+          (ppNTrace normalized)
+       $$ hang (text "Error:") 4
+         (ppResult err) )
+       False
+
+  --in _ -- counterexample (msg ++ "\n  program trace: " ++ show normalized) result
--- src/Test/IOTest/Internal/Trace.hs
+++ src/Test/IOTest/Internal/Trace.hs
@@ -6,17 +6,21 @@ module Test.IOTest.Internal.Trace (
   Trace'(..),
   normalize,
   isCoveredBy,
-  showNTrace,
+  ppNTrace,
+  ppNTraceInfo,
   inputs,
+  MatchResult(..),
+  ppResult,
 ) where
 
 import Test.IOTest.Internal.Pattern
 
-import Control.Monad.Trans.Writer
-
-import Data.Set (Set)
+import           Data.Set (Set)
 import qualified Data.Set as S
 import           Data.List
+import           Data.Function ( fix )
+
+import Text.PrettyPrint hiding ((<>))
 
 data Trace' o
   = ProgRead String (Trace' o)
@@ -28,10 +32,6 @@ data Trace' o
 type Trace = Trace' String
 type NTrace = Trace' (Set LinearPattern)
 
-
-ppMismatch :: (Show a1, Show a2) => Set a1 -> Set a2 -> String
-ppMismatch xs ys = "Mismatch while comparing patterns: " ++ show (S.toList xs) ++ " is not covered by " ++ show (S.toList ys)
-
 normalize :: Trace -> NTrace
 normalize = go emptyPattern where
   go p (ProgWrite v t') = go (p <> buildPattern v) t'
@@ -39,27 +39,44 @@ normalize = go emptyPattern where
   go p Stop = ProgWrite (S.singleton p) Stop
   go p OutOfInputs = ProgWrite (S.singleton p) OutOfInputs
 
-isCoveredBy :: NTrace -> NTrace -> Writer String Bool
-ProgRead x t1 `isCoveredBy` ProgRead y t2 =
+data MatchResult
+  = MatchSuccessfull
+  | InputMismatch Doc
+  | OutputMismatch Doc
+  | AlignmentMismatch Doc
+  deriving Show
+
+ppResult :: MatchResult -> Doc
+ppResult MatchSuccessfull = text $ show MatchSuccessfull
+ppResult (InputMismatch msg) = hang (text "InputMismatch:") 4 msg
+ppResult (OutputMismatch msg) = hang (text "OutputMismatch:") 4 msg
+ppResult (AlignmentMismatch msg) = hang (text "AlignmentMismatch:") 4 msg
+
+isCoveredBy :: NTrace -> NTrace -> MatchResult
+t1@(ProgRead x t1') `isCoveredBy` t2@(ProgRead y t2') =
   if x == y
-    then t1 `isCoveredBy` t2
-    else do
-      tell $ "Input mismatch: Expected " ++ show x ++ ". But got " ++ show y
-      return False
-ProgWrite v1 t1' `isCoveredBy` ProgWrite v2 t2' =
-  if v1 `isSubsetOf` v2
     then t1' `isCoveredBy` t2'
-    else do
-      tell $ ppMismatch v1 v2
-      return False
-Stop `isCoveredBy` Stop = return True
-_ `isCoveredBy` _ = tell "traces dont line up" >> return False
+    else InputMismatch (reportExpectationMismatch t2 t1)
+t1@(ProgWrite v1 t1') `isCoveredBy` (ProgWrite v2 t2')
+  | v1 `isSubsetOf` v2 = t1' `isCoveredBy` t2'
+  | S.size v2 == 1 && S.member emptyPattern v2 = InputMismatch $ reportExpectationMismatch t2' t1
+  | otherwise = OutputMismatch $ reportCoverageFailure v1 v2
+Stop `isCoveredBy` Stop = MatchSuccessfull
+t1 `isCoveredBy` t2 = AlignmentMismatch (reportExpectationMismatch t1 t2)
 
 isSubsetOf :: Set LinearPattern -> Set LinearPattern -> Bool
 x `isSubsetOf` y = x == y || all (\px -> any (\py -> px `isSubPatternOf` py) (S.toList y)) (S.toList x)
                           -- '           '        '- such that
                           -- '           '- there exist an element py in y
                           -- '- forall elements px in x
+
+reportCoverageFailure :: Set LinearPattern -> Set LinearPattern -> Doc
+reportCoverageFailure xs ys = text "the set" <+> printSet xs <+> text "is not a subset of" <+> printSet ys
+  where removeBrackets s = s >>= \c -> [c | c `notElem` "[]"]
+        printSet set = braces . text $ removeBrackets $ show (S.toList set)
+
+reportExpectationMismatch :: NTrace -> NTrace -> Doc
+reportExpectationMismatch t1 t2 = text (showNTraceHead Expected t1) $$ text (showNTraceHead Got t2)
 
 instance Show Trace where
   show (ProgRead v t) = "?"++show v++" "++show t
@@ -68,27 +85,42 @@ instance Show Trace where
   show OutOfInputs = "<out of inputs>"
 
 instance Show NTrace where
-  show = showNTrace
+  show = fix showNTraceFF
 
-showNTrace :: NTrace -> String
-showNTrace (ProgRead v t) = "?"++show v++" "++showNTrace t
-showNTrace (ProgWrite v t) =
+showNTraceFF :: (NTrace -> String) -> NTrace -> String
+showNTraceFF ff (ProgRead v t) = "?"++ v++" "++ ff t
+showNTraceFF ff (ProgWrite v t) =
   let vs = (\x -> if x == "[]" then "e" else x) . show <$> S.toList v
-  in "!{"++ intercalate "," vs ++"} "++showNTrace t
-showNTrace Stop = "stop"
-showNTrace OutOfInputs = "<out of inputs>"
+  in "!{"++ intercalate "," vs ++"} "++ ff t
+showNTraceFF _ Stop = "stop"
+showNTraceFF _ OutOfInputs = "<out of inputs>"
+
+data ShowHeadType = Expected | Got | Plain
+
+showNTraceHead :: ShowHeadType -> NTrace -> String
+showNTraceHead Expected t = "Expected: " <> showNTraceHead Plain t
+showNTraceHead Got t = "Got: " <> showNTraceHead Plain t
+showNTraceHead Plain t = showNTraceFF (const "") t
+
+ppNTraceInfo :: NTrace -> Doc
+ppNTraceInfo t = inputSequence $+$ generalizedRun
+  where inputSequence = hang (text "Input sequence:") 4 $ hsep $ (text . ('?' :)) <$> inputs t
+        generalizedRun = hang (text "Expected run (generalized):") 4 $ ppNTrace t
+
+-- ommit occurences of !{}
+ppNTrace :: NTrace -> Doc
+ppNTrace = hcat . fmap text . filter (/= "!{} ") . traceToStringSequence
+
+traceToStringSequence :: NTrace -> [String]
+traceToStringSequence t@(ProgRead _ t') = showNTraceHead Plain t : traceToStringSequence t'
+traceToStringSequence t@(ProgWrite vs t')
+  | S.size vs == 1 && S.member emptyPattern vs = traceToStringSequence t'
+  | otherwise                                  = showNTraceHead Plain t : traceToStringSequence t'
+traceToStringSequence Stop = [showNTraceHead Plain Stop]
+traceToStringSequence OutOfInputs = [showNTraceHead Plain OutOfInputs]
 
 similar :: Trace' a -> Trace' b -> Bool
 similar x y = inputs x == inputs y
-
-ppOutputMismatch :: Show a => [a] -> [a] -> String
-ppOutputMismatch [v1] v2 =
-  let clean :: Show a => a -> String
-      clean = filter (`notElem` ['\\','\"']) . show
-  in case v2 of
-      [v2'] -> "Output mismatch: Expected " ++ clean v2' ++ ". But got " ++ clean v1
-      _ ->  "Output mismatch: Expected one of " ++ clean v2 ++ ". But got " ++ clean v1
-ppOutputMismatch _ _ = error "should not happen"
 
 inputs :: Trace' a -> [String]
 inputs (ProgRead v t) = v : inputs t
