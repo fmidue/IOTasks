00789eae50d0512c5aa31a203e26c2516dc4a38c
Oliver Westphal <oliver.westphal@uni-due.de>
2019-08-28 16:58:32 +0200

    renaming

--- src/Test/IOTest/Translation.hs
+++ src/Test/IOTest/Translation.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 module Test.IOTest.Translation (
@@ -27,41 +26,41 @@ import Control.Monad.Trans.State
 import Control.Monad.Trans.Class
 
 buildProgram :: TeletypeM m => Specification -> m ()
-buildProgram s = void $ evalStateT (translate s) (freshContext s)
+buildProgram s = void $ evalStateT (interpret s) (freshContext s)
 
 -- translates to a 'minimal' program satisfying the specification
-translate :: TeletypeM m => Specification -> StateT Context m LoopEnd
-translate (ReadInput x vs) =
+interpret :: TeletypeM m => Specification -> StateT Context m LoopEnd
+interpret (ReadInput x vs) =
   elimValueSet vs (error "proxy RandomGen sampled" :: StdGen)
     (\ p _ (_ :: ty) -> do
       v <- unpack @_ @ty p <$> lift getLine
       modify (\d -> fromJust $ update d x (Value p v))
       return No
   )
-translate (WriteOutput _ _ [] _) = error "empty list of output options"
-translate (WriteOutput _ True _ _) = return No
-translate (WriteOutput pxy False (p:_) ts) = do
+interpret (WriteOutput _ _ [] _) = error "empty list of output options"
+interpret (WriteOutput _ True _ _) = return No
+interpret (WriteOutput pxy False (p:_) ts) = do
   d <- get
   lift . putStrLn . render . pPrint $ fillHoles pxy p ts d
   return No
-translate E = return Yes
-translate Nop = return No
-translate (TillE s) =
-  let body = translate s
+interpret E = return Yes
+interpret Nop = return No
+interpret (TillE s) =
+  let body = interpret s
       go = do
         end <- body
         case end of
           Yes -> return No
           No -> go
   in go
-translate (Branch p s1 s2) = do
+interpret (Branch p s1 s2) = do
   d <- get
   if evalTerm p d
-    then translate s2
-    else translate s1
-translate (s1 :<> s2) =
-  translate s1 >>=
+    then interpret s2
+    else interpret s1
+interpret (s1 :<> s2) =
+  interpret s1 >>=
     \case Yes -> return Yes
-          No -> translate s2
+          No -> interpret s2
 
 data LoopEnd = Yes | No
