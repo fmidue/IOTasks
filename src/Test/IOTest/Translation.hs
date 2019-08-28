1cd163f6329d04b02653977ecf0b6e4996bc66b5
Oliver Westphal <oliver.westphal@uni-due.de>
2019-08-28 16:53:32 +0200

    use StateT

--- src/Test/IOTest/Translation.hs
+++ src/Test/IOTest/Translation.hs
@@ -22,36 +22,42 @@ import Data.Maybe
 import System.Random
 import Text.PrettyPrint.HughesPJClass
 
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
+
 buildProgram :: TeletypeM m => Specification -> m ()
-buildProgram s = void $ translate s (freshContext s)
+buildProgram s = void $ evalStateT (translate s) (freshContext s)
 
 -- translates to a 'minimal' program satisfying the specification
-translate :: TeletypeM m => Specification -> Context -> m (Context,LoopEnd)
-translate (ReadInput x vs) d =
+translate :: TeletypeM m => Specification -> StateT Context m LoopEnd
+translate (ReadInput x vs) =
   elimValueSet vs (error "proxy RandomGen sampled" :: StdGen)
     (\ p _ (_ :: ty) -> do
-      v <- unpack @_ @ty p <$> getLine
-      return (fromJust $ update d x (Value p v), No)
+      v <- unpack @_ @ty p <$> lift getLine
+      modify (\d -> fromJust $ update d x (Value p v))
+      return No
   )
-translate (WriteOutput _ _ [] _) _ = error "empty list of output options"
-translate (WriteOutput _ True _ _) d = return (d,No)
-translate (WriteOutput pxy False (p:_) ts) d = do
-  putStrLn $ render $ pPrint $ fillHoles pxy p ts d
-  return (d, No)
-translate E d = return (d, Yes)
-translate Nop d = return (d, No)
-translate (TillE s) d =
+translate (WriteOutput _ _ [] _) = error "empty list of output options"
+translate (WriteOutput _ True _ _) = return No
+translate (WriteOutput pxy False (p:_) ts) = do
+  d <- get
+  lift . putStrLn . render . pPrint $ fillHoles pxy p ts d
+  return No
+translate E = return Yes
+translate Nop = return No
+translate (TillE s) =
   let body = translate s
-      go d' = do
-        (d'', end) <- body d'
+      go = do
+        end <- body
         case end of
-          Yes -> return (d'', No)
-          No -> go d''
-  in go d
-translate (Branch p s1 s2) d =
+          Yes -> return No
+          No -> go
+  in go
+translate (Branch p s1 s2) = do
+  d <- get
   if evalTerm p d
-    then translate s2 d
-    else translate s1 d
-translate (s1 :<> s2) d = translate s1 d >>= (\(d',_) -> translate s2 d')
+    then translate s2
+    else translate s1
+translate (s1 :<> s2) = translate s1 >>= (\_ -> translate s2)
 
 data LoopEnd = Yes | No
