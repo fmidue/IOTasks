2661694ed4f23467641db3eb5b23aa4f8f050237
Oliver Westphal <oliver.westphal@uni-due.de>
2019-09-04 11:55:39 +0200

    regression test + replaced fromJust in buildComputation

--- src/Test/IOTest/Translation.hs
+++ src/Test/IOTest/Translation.hs
@@ -27,7 +27,11 @@ import Text.PrettyPrint.HughesPJClass hiding ((<>))
 import Control.Monad.State
 
 buildComputation :: MonadTeletype m => Specification -> m ()
-buildComputation s = fromJust <$> evalSemantics (buildComputation' s) (freshContext s)
+buildComputation s = do
+  loopStatus <- evalSemantics (buildComputation' s) (freshContext s)
+  case loopStatus of
+    Just () -> return ()
+    Nothing -> error "buildComputation: loopEnd at toplevel"
 
 -- translates to a 'minimal' program satisfying the specification
 buildComputation' ::MonadTeletype m => Specification -> Semantics m ()
--- test/RegressionTests.hs
+++ test/RegressionTests.hs
@@ -7,14 +7,18 @@ import Test.IOTest.IOrep
 import Test.IOTest.IOProperty
 import Test.IOTest.Translation
 import Test.IOTest.Language
+import Test.IOTest.Internal.TraceSet
 
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck (within)
+import Test.QuickCheck (within, sample)
 
 regressionTests :: Spec
-regressionTests = describe "Testing for regressions:" $
+regressionTests = describe "Testing for regressions:" $ do
   prop "correctly break loop on end marker in interpretation" $
     let s = tillEnd $ end <> writeFixedOutput [buildPattern "X"]
         p = buildComputation @IOrep s
     in within 1000000 $ p `fulfills` s
+  describe "fail with a runtime error for a toplevel loopEnd in traceGen and buildComputation" $ do
+    specify "for buildComputation" $ buildComputation end `shouldThrow` anyErrorCall
+    specify "for traceGen" $ sample (traceGen end) `shouldThrow` anyErrorCall
