09fd6c98a64340c75b9f6f015507d902a1a7f181
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2019-09-26 17:30:19 +0200

    expose more concrete/explicit type in Term

--- src/Test/IOTest/Internal/Term.hs
+++ src/Test/IOTest/Internal/Term.hs
@@ -15,6 +15,7 @@ module Test.IOTest.Internal.Term (
 import Test.IOTest.Internal.Environment
 import Test.IOTest.Utils
 
+import Data.Functor.Identity (Identity(..))
 import Control.Monad.Reader
 import Control.Monad.Trans.Maybe
 
@@ -22,33 +23,33 @@ import Data.Maybe
 import Data.Dynamic
 import Data.Proxy
 
-newtype Term a = Term { getTerm :: MaybeT (Reader Environment) a }
+newtype Term a = Term { getTerm :: Environment -> Maybe a }
   deriving (Functor, Applicative) via (MaybeT (Reader Environment))
 
 evalTerm :: Term a -> Environment -> a
-evalTerm t = fromMaybe (error "Can not evaluate epsilon!") . (runReader . runMaybeT $ getTerm t)
+evalTerm t = fromMaybe (error "Can not evaluate epsilon!") . getTerm t
 
 --TODO: What is this good for? Janis: I don't think for anything.
 -- update :: Eq v => v -> s -> Term ()
--- update x v = Term $ MaybeT $ Just <$> modify (map (x `addValue` v))
+-- update x v = Term $ Just <$> modify (map (x `addValue` v))
 --   where
 --     addValue x' v' (y,vs') = if y == x' then (y,vs' ++ [v']) else (y,vs')
 
 epsilon :: Term a
-epsilon = Term $ MaybeT $ return Nothing
+epsilon = Term $ return Nothing
 
 isEpsilon :: Term a -> Bool
-isEpsilon t = isNothing $ runReader (runMaybeT $ getTerm t) []
+isEpsilon t = isNothing $ getTerm t []
 
 getCurrent :: forall s a . (Typeable a, StringEmbedding s a) => Proxy s -> Varname -> Term a
 getCurrent p x =
   let vs = getAll p x
-  in if isJust $ runReader (runMaybeT $ getTerm vs) []
+  in if isJust $ getTerm vs []
     then last <$> vs
     else error $ "getCurrent: no values stored for " <> x
 
 getAll :: forall s a . (Typeable a, StringEmbedding s a) => Proxy s -> Varname -> Term [a]
-getAll p x = Term . MaybeT $ Just <$> do
+getAll p x = Term $ Just <$> do
   mVs <- reader $ lookupNameAtType p x
   case mVs of
     Left e -> error $ printLookupError e
