4ba3614d2d619cb8fdf2b63fae2c96e7cfce412b
Oliver Westphal <oliver.westphal@uni-due.de>
2019-10-29 13:34:15 +0100

    use monad transformer stack for specification generation

--- test/SpecGen.hs
+++ test/SpecGen.hs
@@ -1,135 +1,158 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
 module SpecGen (specGen, loopBodyGen) where
 
-import Test.IOTest.Language (tillExit, exit, ints, nats)
+import Test.IOTest.Language (exit, ints, nats)
 import Test.IOTest.Specification
 import Test.IOTest.ValueSet
 import Test.IOTest.Environment
 import Test.IOTest.Pattern
 import Test.IOTest.Term
 
-import Test.QuickCheck hiding (output)
-import           Data.List                      ( nub )
-import           Control.Monad.Extra            ( ifM )
+import Test.QuickCheck (arbitrary, Gen)
+import Test.QuickCheck.GenT
+import           Data.List                      ( nub
+                                                , intersect
+                                                )
+
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.Functor.Identity
 
 specGen :: Gen Specification
 specGen =
   let vs = ["m","n","x","y","z"]
       vss = [ints, nats]
-  in specGen' vs [] vss
+  in runIdentity <$>runGenT (runReaderT (evalStateT specGen' []) (vs,vss))
 
 loopBodyGen :: Gen Specification
-loopBodyGen = sized $ \n ->
+loopBodyGen =
   let vs = ["m","n","x","y","z"]
       vss = [ints, nats]
-  in if n <= 2
-    then return $ exit
+  in sized $ \n -> if n <= 2
+    then return exit
     else do
-      ~(TillE s) <- loop vs [] vss
+      ~(TillE s) <- runIdentity <$> runGenT (runReaderT (evalStateT loop []) (vs,vss))
       return s
-      
-specGen' :: [Varname] -> [Varname] -> [ValueSet] -> Gen Specification
-specGen' vs used vss = sized $ \n ->
+
+type GenM a = StateT [Varname] (ReaderT ([Varname], [ValueSet]) (GenT Identity)) a
+
+specGen' :: GenM Specification
+specGen' = sized $ \n ->
   if n > 0
     then do
-      (s, n', used') <- oneof $
-        [simple vs used vss  | n > 0 ] ++
-        [complex vs used vss | n >= 2]
-      s' <- scale (subtract n') (specGen' vs used' vss)
+      (s, n') <- oneof $
+        [simple  | n > 0 ] ++
+        [complex | n >= 2]
+      s' <- resize (n - n') specGen'
       return $ s <> s'
     else
       return $ Spec []
 
-simple :: [Varname] ->  [Varname] -> [ValueSet] -> Gen (Specification, Int, [Varname])
-simple vs used vss = do
-  (a, used') <- oneof [input vs used vss, output vs used]
-  return (Spec [a], 1, used')
+simple :: GenM (Specification, Int)
+simple = do
+  a <- oneof [input, output]
+  return (Spec [a], 1)
 
-complex :: [Varname] -> [Varname] -> [ValueSet] -> Gen (Specification, Int, [Varname])
-complex vs used vss = sized $ \n -> do
+complex :: GenM (Specification, Int)
+complex = sized $ \n -> do
   n' <- choose (2,n)
-  a <- resize n' $ oneof $
-    [ branch vs used vss | always ] ++
-    [ loop vs used vss   | n' > 2  ]
-  return (Spec [a], n', used)
+  a <- resize n' . oneof $
+    [ branch | always ] ++
+    [ loop   | n' > 2 ]
+  return (Spec [a], n')
 
-input :: [Varname] -> [Varname] -> [ValueSet] -> Gen (Action, [Varname])
-input xs used vss = do
+input :: GenM Action
+input = do
+  (xs,vss) <- ask
   x <- elements xs
   vs <- elements vss
-  return (ReadInput x vs, nub $ x:used)
+  modify (nub.(x:))
+  return $ ReadInput x vs
 
-output :: [Varname] -> [Varname] -> Gen (Action, [Varname])
-output vs used = do
-  opt <- arbitrary @Bool
-  n <- oneof [return 1, choose @Int (2,4)]
+output :: GenM Action
+output = do
+  opt <- liftGen $ arbitrary @Bool
+  n <- oneof [return 1, choose (2::Int,4)]
   let ps = [ buildTermPattern $ '#':show i | i <- [0 .. n-1] ]
-  ts <- vectorOf n (term vs used)
-  return (WriteOutput opt ps ts, used)
+  ts <- vectorOf n term
+  return $ WriteOutput opt ps ts
 
-term :: [Varname] -> [Varname] -> Gen (Term Int)
-term vs used = oneof $ concat
-  [ [ unary  | always          ]
-  , [ var    | not $ null used ]
-  , [ binary | not $ null used ]
-  ] where
+term :: GenM (Term Int)
+term = do
+  used <- get
+  oneof $ concat
+    [ [ unary  | always          ]
+    , [ var    | not $ null used ]
+    , [ binary | not $ null used ]
+    ] where
   var = do
+    used <- get
     x <- elements used
     return $ getCurrent x
   unary = do
+    (vs,_) <- ask
     f <- elements [sum, length]
     x <- elements vs
     return $ f <$> getAll x
   binary = do
+    used <- get
     f <- elements [(+), (-), (*)]
     x <- elements used
     y <- elements used
     return $ f <$> getCurrent x <*> getCurrent y
 
-branch :: [Varname] -> [Varname] -> [ValueSet] -> Gen Action
-branch vs used vss = sized $ \n -> do
-  c <- condition vs used
+branch :: GenM Action
+branch = sized $ \n -> do
+  c <- condition
   ~[n1,n2] <- splitSizeIn 2 (n-1)
-  s1 <- resize n1 (specGen' vs used vss)
-  s2 <- resize n2 (specGen' vs used vss)
+  (s1,s2) <- forkStateComp
+    intersect
+    (resize n1 specGen')
+    (resize n2 specGen')
   return $ Branch c s1 s2
 
-condition :: [Varname] -> [Varname] -> Gen (Term Bool)
-condition vs used = oneof [unary, binary] where
+condition :: GenM (Term Bool)
+condition = oneof [unary, binary] where
   unary = do
+    vs <- asks fst
     x <- elements vs
     return $ null <$> getAll @Int x
   binary = do
     op <- elements [(==), (>), (<)]
-    t1 <- term vs used
-    t2 <- term vs used
+    t1 <- term
+    t2 <- term
     return $ op <$> t1 <*> t2
 
 -- this only generates loops with exactly one exit marker.
-loop :: [Varname] -> [Varname] -> [ValueSet] -> Gen Action
-loop vs used vss = sized $ \n -> do
-  ~[preLen, loopLen, postLen] <- splitSizeIn 3 (n-3)
-  prefix <- resize preLen (specGen' vs used vss)
-  postfix <- resize postLen (specGen' vs used vss)
-  (c,x) <- loopCondition vs
+loop :: GenM Action
+loop = sized $ \n -> do
+  vss <- asks snd
+  ~[preLen, loopLen] <- splitSizeIn 2 (n-3)
+  prefix <- resize preLen specGen'
+  (c,x) <- loopCondition
   ty <- elements vss
   ~[n1, n2] <- splitSizeIn 2 loopLen
   let progress = ReadInput x ty
-  s1 <- resize n1 (specGen' vs used vss)
-  s2 <- resize n2 (specGen' vs used vss)
+  (s1,s2) <- forkStateComp
+    intersect
+    (resize n1 specGen')
+    (resize n2 specGen')
   s1' <- insert progress s1
-  s <- ifM (arbitrary @Bool)
-    (return $ Branch c s1' (s2 <> exit))
-    (return $ Branch (not <$> c) (s2 <> exit) s1')
-  return $ TillE $ prefix <> Spec [s] <> postfix
+  s <- oneof
+    [ return $ Branch c s1' (s2 <> exit)
+    , return $ Branch (not <$> c) (s2 <> exit) s1'
+    ]
+  return $ TillE $ prefix <> Spec [s]
 
-loopCondition :: [Varname] -> Gen (Term Bool, Varname)
-loopCondition vs = do
+loopCondition :: GenM (Term Bool, Varname)
+loopCondition = do
+  vs <- asks fst
   x <- elements vs
-  n <- choose @Int (0,10)
+  n <- choose (0,10)
   return ((\xs i -> length xs > i) <$> getAll @Int x <*> pure n, x)
 
-insert :: Action -> Specification -> Gen Specification
+insert :: Action -> Specification -> GenM Specification
 insert a (Spec as) = do
   ix <- choose (0,length as)
   return . Spec $ take ix as ++ [a] ++ drop ix as
@@ -138,8 +161,33 @@ always :: Bool
 always = True
 
 -- splits (non-negative) n into k (non-negative) values s.t. sum [n_1,..,n_k] == n
-splitSizeIn :: Int -> Int -> Gen [Int]
+splitSizeIn :: Int -> Int -> GenM [Int]
 splitSizeIn k n = elements $ go k n where
   go :: Int -> Int -> [[Int]]
   go 1 n = [[n]]
   go j m = concat [ (i:) <$> go (j-1) (m-i) | i <- [0..m] ]
+
+instance MonadGen m => MonadGen (ReaderT ([Varname],[ValueSet]) m) where
+  liftGen g = lift $ liftGen g
+  variant n = mapReaderT (variant n)
+  sized f = let g r = sized (\n -> runReaderT (f n) r) in ReaderT g
+  resize n = mapReaderT (resize n)
+  choose p = lift $ choose p
+
+instance MonadGen m => MonadGen (StateT [Varname] m) where
+  liftGen g = lift $ liftGen g
+  variant n = mapStateT (variant n)
+  sized f = let g s = sized (\n -> runStateT (f n) s) in StateT g
+  resize n = mapStateT (resize n)
+  choose p = lift $ choose p
+
+forkStateComp :: MonadState s m => (s -> s -> s) -> m a -> m b -> m (a,b)
+forkStateComp f ma mb = do
+  st <- get -- store current state
+  a <- ma -- execute ma in st
+  stA <- get
+  put st -- restore original state
+  b <- mb -- execute mb in st
+  stB <- get
+  put (f stA stB) -- combine resulting states of ma and mb
+  return (a,b)
