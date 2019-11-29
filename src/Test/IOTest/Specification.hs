7de11318090710992ec72acb868d36dfc3e65082
Oliver Westphal <oliver.westphal@uni-due.de>
2019-11-29 10:27:54 +0100

    Pretty instance for Action

--- src/Test/IOTest/Specification.hs
+++ src/Test/IOTest/Specification.hs
@@ -23,6 +23,8 @@ import Data.List (nub)
 import Data.MonoTraversable
 import Data.MonoTraversable.Unprefixed (foldr)
 
+import Text.PrettyPrint.Annotated.HughesPJClass hiding ((<>))
+
 newtype Specification = Spec [Action]
   deriving (Semigroup, Monoid, MonoFoldable) via [Action]
 
@@ -40,11 +42,14 @@ instance Show Specification where
   show (Spec as) = show as
 
 instance Show Action where
-  show (ReadInput x _) = "ReadInput " ++ show x ++ " _"
-  show (WriteOutput b ps ts) = concat ["WriteOutput ", show b, " ", show ps, " _{"++ show (vars <$> ts) ++"}"]
-  show (Branch c s1 s2) = concat ["Branch _{",show $ vars c,"} ", show s1, " ", show s2]
-  show (TillE s) = "TillE " ++ show s
-  show E = "E"
+  show = render . pPrint
+
+instance Pretty Action where
+  pPrint (ReadInput x _) = text "ReadInput" <+> text (show x) <+> text "_"
+  pPrint (WriteOutput b ps ts) = hsep [text "WriteOutput", text (show b), text (show ps), text "_{", text (show (vars <$> ts)), text "}"]
+  pPrint (Branch c s1 s2) = hang (text "Branch _{" <+> text (show $ vars c) <+> text "}") 2 (text (show s1) $+$ text (show s2))
+  pPrint (TillE s) = hang (text "TillE") 2 (text (show s))
+  pPrint E = text "E"
 
 -- move into Combinators ?
 optional :: Specification -> Specification
