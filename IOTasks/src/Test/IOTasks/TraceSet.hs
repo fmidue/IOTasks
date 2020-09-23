8095dffc857ff826f443ceae357a3b22be6b201c
Oliver Westphal <oliver.westphal@uni-due.de>
2020-09-23 15:03:08 +0200

    parameterize traceSet over choice

--- IOTasks/src/Test/IOTasks/TraceSet.hs
+++ IOTasks/src/Test/IOTasks/TraceSet.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -22,17 +23,20 @@ import Test.QuickCheck.GenT
 
 import qualified Data.Set as S
 import           Data.Maybe
+import           Control.Monad                  ( ap )
 
 traceGen :: forall t m. (MonadGen m, SemTerm t (Environment Varname), VarListTerm t Varname) => Specification t -> m GeneralizedTrace
-traceGen (Spec s) = traceSet s kTI (freshEnvironment $ specVars (Spec s)) where
-  kTI End  _ = return StopN
-  kTI Exit _ = error "traceGen: 'throwError Exit' at toplevel"
+traceGen (Spec s) = chooseTrace valueOf $ traceSet s kTI (freshEnvironment $ specVars (Spec s)) where
 
-traceSet :: SemTerm t (Environment Varname) => MonadGen m => [Action (Specification t) t] -> (Cont -> Environment Varname -> m GeneralizedTrace) -> Environment Varname -> m GeneralizedTrace
-traceSet (ReadInput x ty : s') k e = do
-  v <- valueOf ty
-  let e' = fromMaybe (error "type mismatch on environment update") (storeValue x v e)
-  fmap (ProgReadN (show v) StopN <>) (traceSet s' k e')
+kTI :: Applicative f => Cont -> a -> f GeneralizedTrace
+kTI End  _ = pure StopN
+kTI Exit _ = error "traceGen: 'throwError Exit' at toplevel"
+
+traceSet :: SemTerm t (Environment Varname) => [Action (Specification t) t] -> (Cont -> Environment Varname -> TraceTree) -> Environment Varname -> TraceTree
+traceSet (ReadInput x ty : s') k e =
+  Bind $ ValueChoice ty $ \v ->
+    let e' = fromMaybe (error "type mismatch on environment update") (storeValue x v e)
+    in fmap (ProgReadN (show v) StopN <>) (traceSet s' k e')
 traceSet (WriteOutput opt ps ts : s') k e =
   let v1 = S.fromList ((\p -> fillHoles (p,ts) e) <$> ps)
       v1' = MkMergeSet $ if opt then S.insert emptyPattern v1 else v1
@@ -73,3 +77,38 @@ sampleNTrace (ProgWriteReadN (MkMergeSet vs) v t2) = do
   return $ if p == emptyPattern
     then ProgRead v t1
     else foldr ProgWrite Stop vs' <> ProgRead v t1
+
+-- new implementation
+data Free f a
+  = Return a
+  | Bind (f (Free f a))
+  deriving Functor
+
+instance (Functor f) => Applicative (Free f) where
+  pure = return
+  (<*>) = ap
+
+instance (Functor f) => Monad (Free f) where
+  return = Return
+  (Return a) >>= f = f a
+  (Bind m) >>= f = Bind ((>>= f) <$> m)
+
+data ValueChoice a = ValueChoice { valueSet :: ValueSet , chooseValue :: Value -> a }
+  deriving Functor
+
+type TraceTree = Free ValueChoice GeneralizedTrace
+
+chooseTrace :: (Monad m) => (ValueSet -> m Value) -> TraceTree -> m GeneralizedTrace
+chooseTrace f = chooseTrace' (const f)
+
+chooseTrace' :: (Monad m) => (Int -> ValueSet -> m Value) -> TraceTree -> m GeneralizedTrace
+chooseTrace' f = go 0 where
+  go _ (Return t) = pure t
+  go i (Bind (ValueChoice ty cont)) = f i ty >>= (go (i+1) . cont)
+
+genTraceForSequence :: (SemTerm t (Environment Varname), VarListTerm t Varname) => [Value] -> Specification t -> Maybe GeneralizedTrace
+genTraceForSequence is (Spec as) = chooseTrace' maybeChoose $ traceSet as kTI (freshEnvironment $ specVars (Spec as))
+  where
+    maybeChoose i ty =
+      let v = (is !! i)
+      in if ty `containsValue` v then Just v else Nothing
