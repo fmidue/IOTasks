021db61be916bfefa5f8e83b5865dd1e90420538
Oliver Westphal <oliver.westphal@uni-due.de>
2021-06-08 13:00:02 +0200

    remove size reduction

--- IOTasks/test/TestPTermExamples.hs
+++ IOTasks/test/TestPTermExamples.hs
@@ -34,7 +34,7 @@ testPTermExamples = describe "Testing Test.IOTasks.Examples.PTermExamples:" $ do
   testPairSuccess (solution3,"solution3") (ex3P,"ex3P")
 
   -- Example 4
-  modifyMaxSize (const 30) $ testPairSuccess (sum2,"sum2") (sum2Spec,"sum2Spec")
+  testPairSuccess (sum2,"sum2") (sum2Spec,"sum2Spec")
 
   -- Generation of programs
   prop "program generated from ex1P matches ex1P" $
