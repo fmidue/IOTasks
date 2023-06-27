6e1de7acf48fb7a4e89d6c9dacd8336aeebeb131
Oliver Westphal <oliver.westphal@uni-due.de>
2023-06-27 14:55:19 +0200

    fix IOTask.Random exports

--- src/IOTasks/Random.hs
+++ src/IOTasks/Random.hs
@@ -1,6 +1,7 @@
 module IOTasks.Random
-  ( Specification, runSpecification
-  , readInput, writeOutput, writeOptionalOutput, optionalTextOutput, branch, tillExit, exit, until, while
+  ( Specification, runSpecification, runSpecification', accept
+  , readInput, writeOutput, writeOptionalOutput, optionalTextOutput, branch, tillExit, exit, nop, until, while
+  , pPrintSpecification
   , InputMode(..)
   , MonadTeletype(..)
   , IOrep, runProgram, Line
@@ -9,13 +10,16 @@ module IOTasks.Random
   , ValueSet(..)
   , OutputPattern(..), PatternType(..)
   , OutputTerm, filter'
+  , Var, Varname, var, intVar, stringVar
   , (.+.), (.-.), (.*.), intLit
   , (.==.), (.>.), (.>=.), (.<.), (.<=.)
-  , (.&&.), (.||.), not'
-  , sum', length', product'
-  , currentValue, allValues
+  , (.&&.), (.||.), not', true, false
+  , sum', length', reverse', product', listLit
+  , isIn, isNotIn
+  , currentValue, allValues, currentValue', allValues', as
+  , OverflowType
   , taskCheck, taskCheckWith, taskCheckOutcome, taskCheckWithOutcome, Args(..), stdArgs
-  , Outcome(..), isSuccess, overflowWarnings
+  , Outcome(..), CoreOutcome(..), OutcomeHints(..), isSuccess, isFailure, overflowWarnings
   , pPrintOutcome, pPrintOutcomeSimple
   , generateStaticTestSuite, taskCheckOn
   , genInput
