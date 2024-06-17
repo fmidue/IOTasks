5738cda64d5658dcaa0aff473f06853a26f6702d
Oliver Westphal <oliver.westphal@uni-due.de>
2024-06-17 11:36:33 +0200

    spelling

--- examples/BranchingStrictnessSpec.hs
+++ examples/BranchingStrictnessSpec.hs
@@ -4,12 +4,12 @@ import Test.IOTasks
 import Test.Hspec
 import Test.IOTasks.Trace (ordinaryTrace)
 
-specificaiton :: Specification
-specificaiton =
+specification :: Specification
+specification =
   readInput x ints AssumeValid <>
   branch (currentValue x .==. intLit 1)
     nop
-    (readInput x undefined undefined) -- cannot be completely undefined as we do one complete travesal for the initial variable environment
+    (readInput x undefined undefined) -- cannot be completely undefined as we do one complete traversal for the initial variable environment
   where
     x = intVar "x"
 
@@ -17,6 +17,6 @@ spec :: Spec
 spec =
   describe "branch" $ it "runSpecification does not evaluate unneeded branches" $
     let
-      (t1,_) = runSpecification specificaiton ["1"]
-      (t2,_) = runSpecification specificaiton ["1","2"]
+      (t1,_) = runSpecification specification ["1"]
+      (t2,_) = runSpecification specification ["1","2"]
     in ordinaryTrace t1 == ordinaryTrace t2
